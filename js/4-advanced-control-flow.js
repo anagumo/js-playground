24d5f51db6e64782ded131c4cc48a097a445f8d0
Ariana Nagumo <arnagumo@icloud.com>
2020-03-10 21:38:10 -0600

    Indentation fixed

--- js/4-advanced-control-flow.js
+++ js/4-advanced-control-flow.js
@@ -2,21 +2,21 @@
 // 4a For Loop
 
 for (var i = 0; i <= 10; i++) {
-	console.log(`i es ${i}`)
+  console.log(`i es ${i}`)
 }
 
 for (var i = 1; i <= 10; i++) {
-	console.log(`90 * ${i} = ${90 * i}`);
+  console.log(`90 * ${i} = ${90 * i}`);
 }
 
 // 4b Mini exercices
 
 for (var i = 1; i <= 10; i++) {
-	if (i % 2 == 0) {
-		console.log(`${i} es par`);
-	} else {
-		console.log(`${i} es impar`);
-	}
+  if (i % 2 == 0) {
+    console.log(`${i} es par`);
+  } else {
+    console.log(`${i} es impar`);
+  }
 }
 
 // 1 2 3 4 5 6 7 8 9 10 Serie
